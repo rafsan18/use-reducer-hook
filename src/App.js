df1eaba44e7266f28ea917fe652ec9d4dfa61082
rafsan <rafsanamin18@gmail.com>
2020-09-14 21:34:31 +0600

    finish 44

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import ReducerCount from "./components/ReducerCount/ReducerCount";
 
+import PatientManagement from "./components/PatientManagement/PatientManagement";
+
 function App() {
   return (
     <div className="App">
       <ReducerCount></ReducerCount>
+      <PatientManagement></PatientManagement>
     </div>
   );
 }
---
+++ src/components/PatientReducer/PatientReducer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const initialState = {state.count}
+const PatientReducer = () => {
+
+  return (
+    <div>
+      <h1>Manage Doctor Chamber</h1>
+    </div>
+  );
+};
+
+export default PatientReducer;
